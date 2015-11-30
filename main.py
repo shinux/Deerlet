ab4f0b7dd0c44df7db2e5b7a0a37e08edefece07
Sinux <wshmelo@gmail.com>
2015-11-30 20:59:41 +0800

    Typo fix

--- main.py
+++ main.py
@@ -3,9 +3,9 @@
 
 import os
 
-
 from flask import Flask
 from flask import render_template
+
 from config import config
 
 
@@ -34,5 +34,15 @@ def admin():
     return render_template('admin.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
